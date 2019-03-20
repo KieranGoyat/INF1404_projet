4388ba53ca93f6652f0c80c63147e23a0b01993a
e1700219 <e1700219@172.24.53.69>
2019-03-20 17:29:59 +0100

    V1

--- Pantominos/src/Pantominos/PantominosApp.java
+++ Pantominos/src/Pantominos/PantominosApp.java
@@ -5,9 +5,10 @@ public class PantominosApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World !!!");
-		//test3tretre
-		///fqgqggqsqdqd
+		
+		System.out.println("bonjour, je suis un programme remplissant des plateau avec des pentaminos, je serais votre chose durant le deroulement des operations. Que voulez vous faire?");
+	
+		
 	}
 
 }
